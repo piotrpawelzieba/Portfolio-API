ac6e3d19b4ee6122d60de2408b4c674904e9082f
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-01-14 20:10:31 +0100

    view model changed to display uploaded images in the index view

--- viewModels/index.js
+++ viewModels/index.js
@@ -1,4 +1,16 @@
-module.exports = {
-    Title: "My portfolio repository",
-    Subtitle: "Upload image"
+const Photo = require('../models/Photo');
+const path = require('path');
+console.log(path);
+debugger;
+module.exports = (req, res) => {
+    Photo.find({}, (err, response) => {
+        const photos = response.map(({title, url, category, isPublic})=>({title, url, category, isPublic}));
+        console.log(photos);
+        const viewBag = {
+            Title:"Photo Repository", 
+            Subtitle:"Upload photo",
+            photos
+        };
+        res.render('../views/pages/index.ejs', viewBag);
+    });
 }
