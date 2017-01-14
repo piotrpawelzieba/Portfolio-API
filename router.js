44fdb421caa9e74bc237e54a9937a005af1f579d
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-01-14 20:09:50 +0100

    Uploading file function implemented

--- router.js
+++ router.js
@@ -1,15 +1,15 @@
 const path = require('path');
+const express = require('express');
 const indexVM = require('./viewModels');
-const {getPhotos, getPhotoById, getPhotoByCategory, postPhoto, deletePhoto} = require('./controllers/photoController');
+const {getPhotos, getPhotoById, getPhotoByCategory, postPhoto, deletePhoto, uploadFile} = require('./controllers/photoController');
+
 
 module.exports = function(app) {
-    app.get('/', (req, res) => {
-        res.render(path.join(__dirname + '/views/pages/index'), indexVM);
-
-    });
+    app.use('/Assets/uploads', express.static(__dirname + '/Assets/uploads'));
+    app.get('/', indexVM);
     app.get('/api/photos', getPhotos);
     app.get('/api/photos/:id', getPhotoById);
     app.get('/api/photos/:category', getPhotoByCategory);
     app.delete('/api/photos/:id', deletePhoto);
-    app.post('/api/photos', postPhoto);
+    app.post('/api/photos', uploadFile, postPhoto);
 }
