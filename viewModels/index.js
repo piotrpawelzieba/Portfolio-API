e90e58614112d1cdf8ee5718c156a640509a0965
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-01-16 08:52:00 +0100

    console.log and debugger deleted

--- viewModels/index.js
+++ viewModels/index.js
@@ -1,11 +1,9 @@
 const Photo = require('../models/Photo');
 const path = require('path');
-console.log(path);
-debugger;
+
 module.exports = (req, res) => {
     Photo.find({}, (err, response) => {
         const photos = response.map(({title, url, category, isPublic})=>({title, url, category, isPublic}));
-        console.log(photos);
         const viewBag = {
             Title:"Photo Repository", 
             Subtitle:"Upload photo",
