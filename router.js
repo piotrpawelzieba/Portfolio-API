77d808aef7eded1b1d606b7f9414ae5cc360b4f0
Piotr Zieba <1piotr.zieba1@gmail.com>
2017-01-17 10:31:53 +0100

    authentication middlewares implemented

--- router.js
+++ router.js
@@ -2,12 +2,19 @@ const path = require('path');
 const express = require('express');
 const indexVM = require('./viewModels');
 const {getPhotos, getPhotoById, getPhotoByCategory, postPhoto, deletePhoto, uploadFile} = require('./controllers/photoController');
+const {signup, signin} = require('./controllers/authenticationController');
+const passportService = require('./services/passport');
+const passport = require('passport');
+const requireAuth = passport.authenticate('jwt', {session: false});
+const requireSignin = passport.authenticate('local', {session: false});
 
 
 module.exports = function(app) {
     app.use('/Assets/uploads', express.static(__dirname + '/Assets/uploads'));
-    app.get('/', indexVM);
-    app.get('/api/photos', getPhotos);
+    app.get('/', requireAuth, indexVM);
+    app.post('/api/signup', signup);
+    app.post('/api/signin', requireSignin, signin);
+    app.get('/api/photos', requireAuth, getPhotos);
     app.get('/api/photos/:id', getPhotoById);
     app.get('/api/photos/:category', getPhotoByCategory);
     app.delete('/api/photos/:id', deletePhoto);
