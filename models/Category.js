454a2db8c159b85d7df4d215122812a13de44ad1
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-02-11 16:32:59 +0100

    implemented Category schema

---
+++ models/Category.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+    title: {
+        type: String,
+        lowercase: true
+    },
+    isPrivate: Boolean
+});
+
+const Category = mongoose.model("Category", CategorySchema);
+
+module.exports = Category;
